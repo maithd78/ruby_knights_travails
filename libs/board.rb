56b341b412ec65624f65256b6fe92ba6f564aa67
Maithree de Silva <90709583+maithd78@users.noreply.github.com>
2025-06-13 12:44:28 +0900

    add sudocode

---
+++ libs/board.rb
@@ -0,0 +1,7 @@
+# initialize a standard chess board where each position on the board is represented
+# by [x,y] coordinates. 
+# also initialize a standard chess knight with a current position attribute and a possible moves
+# attribute. when a current possition is given to the knight, the knight will calculate all possible moves from
+# that position(max of 8). when a end position is given, the knight will start calculating which moves will 
+# travel to the end position.
+# when knight_moves is called with the current position and end position the knight will move 
